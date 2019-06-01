5c8ec916652a6043eaee05703b3c59f46c7c245a
teonapster <teonapster@localhost.localdomain>
2019-06-01 11:25:17 -0400

    add cors headers

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ console.log(`only user ${process.env.USERNAME} is granted`)
 const server = http.createServer(function (req, res) {
     const credentials = auth(req)
     const urlParts = url.parse(req.url, true);
+    res.setHeader('Access-Control-Allow-Origin', 'example.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     // Check credentials
     // The "check" function will typically be against your user store
